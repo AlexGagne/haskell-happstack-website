ef22d5291ba6df161e128614c0ad71dabdb07ee5
Alex Gagné <alex_gagne94@hotmail.com>
2016-11-02 14:12:10 -0400

    fixing after bad merge

--- src/AlexDb.hs
+++ src/AlexDb.hs
@@ -8,11 +8,6 @@ Maintainer  :  <AlexGagne>
 This file gives access to the MongoDB containing most of the text and data associated with the website.
 -}
 
-module AlexDB where
-
-import qualified System.Environment as E
-import qualified Data.Text as T
-
 module AlexDb where
 
 import qualified Data.Text          as T
@@ -24,30 +19,8 @@ getDatabaseInformation dbValue = DatabaseInfo dbUser dbPassword dbHost dbDatabas
     dbPassword = parsePassword dbValue
     dbHost = parseHost dbValue
     dbDatabaseName = parseDatabaseName dbValue
-
+    
 parseUser :: String -> String
-parseUser dbValue = T.drop 2 $ (T.splitOn (T.pack ":") $ T.pack dbValue !! 1
-
-parsePassword :: String -> String
-parsePassword dbValue = "user"
-
-parseHost :: String -> String
-parseHost dbValue = T.unpack $ (T.splitOn (T.pack "/") $ (T.splitOn (T.pack "@") $ T.pack dbValue) !! 1) !! 0
-
-parseDatabaseName :: String -> String
-parseDatabaseName dbValue = "user"
-
-getBDValue :: IO String
-getBDValue = E.getEnv "MONGODB_URI"
-
-
-data DatabaseInfo = DatabaseInfo{
-    user         :: String,
-    password     :: String,
-    host         :: String,
-    databaseName :: String } deriving (Show, Eq)
-
---mongodb://dbuser:dbpass@host:port/dbname
 parseUser dbValue = T.unpack $ T.splitOn (T.pack "//")
                               (T.splitOn (T.pack ":") (T.pack dbValue) !! 1) !! 1
 
